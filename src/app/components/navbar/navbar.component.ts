db1b19260d9d6566c128d5b02172dd15ff5903a5
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2024-02-13 18:37:09 +0400

    add navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  isNavbarOpen = false;
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
+  constructor() { }
+}
