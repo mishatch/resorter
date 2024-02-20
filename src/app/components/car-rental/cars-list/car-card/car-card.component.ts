c135854cf466b91d3d62b1ffacab8cd859d074bc
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2024-02-20 07:23:06 +0400

    add cars modal

--- src/app/components/car-rental/cars-list/car-card/car-card.component.ts
+++ src/app/components/car-rental/cars-list/car-card/car-card.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject, Input, TemplateRef} from '@angular/core';
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {NgbCarouselConfig, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-car-card',
@@ -7,14 +7,40 @@ import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
   styleUrl: './car-card.component.scss'
 })
 export class CarCardComponent {
-  @Input() image!: any;
+  @Input() image!: string;
   @Input() name!: string;
   @Input() price!: number;
-
-
+  @Input() allImages!: any;
+  @Input() gearbox!: string;
+  @Input() engine!: string;
+  @Input() model!: string;
+  @Input() year!: string;
+  @Input() seats!: string;
+  @Input() drive!: string;
+  @Input() hp!: number;
+  @Input() airbags!: number;
+  @Input() airConditioning!: string;
+  @Input() roof!: string;
+  @Input() tank!: string;
+  @Input() fuel!: string;
+  @Input() wheelSide!: string;
+  @Input() consumption!: string;
+  @Input() interior!: string;
+  @Input() windows!: number;
+  @Input() abs!: boolean;
+  @Input() esp!: boolean;
+  @Input() ebd!: boolean;
+  @Input() cruiseControl!: boolean;
+  @Input() parkingAssist!: boolean;
+  @Input() camera!: boolean;
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+  }
   private modalService = inject(NgbModal);
   openVerticallyCentered(content: TemplateRef<any>) {
     this.modalService.open(content, { centered: true, size: "lg" }, );
   }
-
+  showNavigationArrows = true;
+  showNavigationIndicators = false;
 }
