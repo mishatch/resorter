7b6fd240c80e38ce6503f895a35436e0455e5d1e
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2024-03-10 17:35:44 +0400

    add validations

--- src/app/components/car-rental/filter/filter-validator.ts
+++ src/app/components/car-rental/filter/filter-validator.ts
@@ -1,21 +1,57 @@
-import { AbstractControl, ValidatorFn, FormGroup } from '@angular/forms';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-export function priceRangeValidator(control: AbstractControl): { [key: string]: boolean } | null {
-    const minPrice = control.get('minPrice')?.value;
-    const maxPrice = control.get('maxPrice')?.value;
-
-    if (minPrice > maxPrice) {
-        return { priceMismatch: true };
-    }
-    return null;
+export function dateNotEarlierThanTodayValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const selectedDate = new Date(control.value);
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        if (selectedDate <= today) {
+            return { 'dateNotEarlierThanToday': true };
+        }
+        return null;
+    };
 }
 
-export function dateRangeValidator(control: AbstractControl): { [key: string]: boolean } | null {
-    const startDate = control.get('startDate')?.value;
-    const endDate = control.get('endDate')?.value;
 
-    if (startDate > endDate) {
-        return { dateMismatch: true };
-    }
-    return null;
+
+export function startDateEarlierThanEndDateValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const startDate = new Date(control.get('start_date')?.value);
+        const endDate = new Date(control.get('end_date')?.value);
+        if (startDate > endDate) {
+            return { 'startDateNotEarlierThanEndDate': true };
+        }
+        return null;
+    };
+}
+export function priceRangeValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const minPrice = control.get('min_price')?.value;
+        const maxPrice = control.get('max_price')?.value;
+        if (minPrice !== null && maxPrice !== null && minPrice >= maxPrice) {
+            return { 'priceRangeInvalid': true };
+        }
+        return null;
+    };
+}
+export function fuelConsumptionRangeValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const minValue = control.get('fuel_consumption_min')?.value;
+        const maxValue = control.get('fuel_consumption_max')?.value;
+        if (minValue !== null && maxValue !== null && minValue >= maxValue) {
+            return { 'fuelConsumptionRangeInvalid': true };
+        }
+        return null;
+    };
+}
+
+export function engineTypeRangeValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const minValue = control.get('engine_type_min')?.value;
+        const maxValue = control.get('engine_type_max')?.value;
+        if (minValue !== null && maxValue !== null && minValue >= maxValue) {
+            return { 'engineTypeRangeInvalid': true };
+        }
+        return null;
+    };
 }
--- src/app/components/car-rental/filter/filter.component.ts
+++ src/app/components/car-rental/filter/filter.component.ts
@@ -1,8 +1,14 @@
 import {Component, inject, TemplateRef} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FilterDataService } from '../../../modules/core/services/filter-data.service';
-import { priceRangeValidator, dateRangeValidator } from './filter-validator';
-import {NgbOffcanvas, OffcanvasDismissReasons} from "@ng-bootstrap/ng-bootstrap";
+import {
+  startDateEarlierThanEndDateValidator,
+  dateNotEarlierThanTodayValidator,
+  priceRangeValidator,
+  fuelConsumptionRangeValidator,
+  engineTypeRangeValidator
+} from './filter-validator';
+import {NgbOffcanvas} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-filter',
@@ -20,13 +26,13 @@ export class FilterComponent {
 
   ngOnInit() {
     this.filterForm = this.formBuilder.group({
-      start_date: [this.minStartDate, Validators.required],
-      end_date: [this.minStartDate, Validators.required],
+      start_date: [this.minStartDate, [Validators.required, dateNotEarlierThanTodayValidator()]],
+      end_date: [this.minStartDate, [Validators.required]],
       min_price: [0, [Validators.min(0), Validators.max(1000)]],
       max_price: [1000, [Validators.min(0), Validators.max(1000)]],
-      body_types: [''],
-      transmission: [''],
-      pick_up: ['თბილისი'],
+      body_types: ['სედანი'],
+      transmission: ['Automatic'],
+      pick_up: ['Tbilisi'],
       fuels: [''],
       drives: [''],
       year: [2000],
@@ -35,7 +41,12 @@ export class FilterComponent {
       engine_type_min: [0, [Validators.min(0), Validators.max(20)]],
       engine_type_max: [20, [Validators.min(0), Validators.max(20)]],
       checkboxes: ['']
-    }, { validator: [priceRangeValidator, dateRangeValidator] });
+    }, { validator: [
+        startDateEarlierThanEndDateValidator(),
+        priceRangeValidator(),
+        fuelConsumptionRangeValidator(),
+        engineTypeRangeValidator()
+      ]});
   }
 
 
@@ -47,11 +58,32 @@ export class FilterComponent {
     }
   }
   clearFilter() {
-    this.filterForm.reset();
     this.filterDataService.setFilterData(null);
+    this.filterForm = this.formBuilder.group({
+      start_date: [this.minStartDate, [Validators.required, dateNotEarlierThanTodayValidator()]],
+      end_date: [this.minStartDate, [Validators.required]],
+      min_price: [0, [Validators.min(0), Validators.max(1000)]],
+      max_price: [1000, [Validators.min(0), Validators.max(1000)]],
+      body_types: ['სედანი'],
+      transmission: ['Automatic'],
+      pick_up: ['Tbilisi'],
+      fuels: [''],
+      drives: [''],
+      year: [2000],
+      fuel_consumption_min: [0, [Validators.min(0), Validators.max(80)]],
+      fuel_consumption_max: [80, [Validators.min(0), Validators.max(80)]],
+      engine_type_min: [0, [Validators.min(0), Validators.max(20)]],
+      engine_type_max: [20, [Validators.min(0), Validators.max(20)]],
+      checkboxes: ['']
+    }, { validator: startDateEarlierThanEndDateValidator() });
   }
   openEnd(content: TemplateRef<any>) {
     this.offcanvasService.open(content, { position: 'end' });
   }
-
+  get end_date() {
+    return this.filterForm.get('end_date');
+  }
+  log(x: any) {
+    console.log(x);
+  }
 }
